26f3dbcdf664b6939e7c046d3910e641697054a2
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-05-08 16:17:51 +0430

    Configuring the Rails API Session Store to Work in All Environments

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_RentAnItem"
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://link-to-your-production-app.com/"
+  else
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+  end
