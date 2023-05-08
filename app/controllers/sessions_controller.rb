f0d510aed4e6a7cf9e01ba633d4853d4c94b338b
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-05-08 16:12:21 +0430

    Add logged and loggout method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+    include CurrentUserConcern
     def create
         user = User
               .find_by(email: params["user"]["email"])
@@ -16,4 +17,25 @@ class SessionsController < ApplicationController
            render json: { status: 401 }
         end
     end
+
+    def logged_in
+        if @current_user
+          render json: {
+            logged_in: true,
+            user: @current_user
+          }
+        else
+          render json: {
+            logged_in: false
+          }
+        end
+      end
+    
+      def logout
+        reset_session
+        render json: { 
+          status: 200, 
+          logged_out: true 
+        }
+      end
 end
