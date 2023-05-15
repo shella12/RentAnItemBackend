f758e319f616c47360d7b691ca936621986d6691
Allassane <dickoallassane1997@gmail.com>
2023-05-15 12:38:44 +0100

    Add test: delete favorite house

--- spec/controllers/api/v1/favorite_house_controller_spec.rb
+++ spec/controllers/api/v1/favorite_house_controller_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Api::V1::FavoriteHousesController, type: :request do
     before(:all) do
       @user = User.first
       @user ||= User.create(email: 'test@gmail.com', password: '123456')
-      house = House.first
-      house ||= House.create(name: 'house1', price: 100, picture: 'picture1', description: 'description1', owner_name: 'owner1')
+      @house = House.first
+      @house ||= House.create(name: 'house1', price: 100, picture: 'picture1', description: 'description1', owner_name: 'owner1')
       favorite = FavoriteHouse.first
       favorite ||= FavoriteHouse.create(user_id: @user.id, house_id: @house.id)
     end
@@ -21,6 +21,11 @@ RSpec.describe Api::V1::FavoriteHousesController, type: :request do
       house ||= House.create(name: 'house2', price: 100, picture: 'picture1', description: 'description1', owner_name: 'owner1')
       post "/api/v1/users/#{@user.id}/favorite_houses/", params: { user_id: @user.id, house_id: house.id }
       expect(response).to have_http_status(:created)
+    end
+
+    it 'should delete a favorite' do
+      delete "/api/v1/users/#{@user.id}/favorite_houses/#{@house.id}/", params: { user_id: @user.id, house_id: @house.id }
+      expect(response).to have_http_status(:success)
     end
 
   end
