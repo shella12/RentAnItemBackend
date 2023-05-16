e7211c47b2a747fcbd7a41e868402958c35fd7da
Allassane <dickoallassane1997@gmail.com>
2023-05-16 14:51:46 +0100

    Update favorite_house_controller_spec: update user to test_user value

--- spec/controllers/api/v1/favorite_house_controller_spec.rb
+++ spec/controllers/api/v1/favorite_house_controller_spec.rb
@@ -3,13 +3,15 @@ require 'rails_helper'
 RSpec.describe Api::V1::FavoriteHousesController, type: :request do
   describe 'test api/v1/favorites endpoint' do
     before(:all) do
-      @user = User.first
+      @user = User.find_by(email: 'test@gmail.com')
       @user ||= User.create(email: 'test@gmail.com', password: '123456')
       @house = House.first
       @house ||= House.create(name: 'house1', price: 100, picture: 'picture1', description: 'description1',
                               owner_name: 'owner1')
 
-      FavoriteHouse.create(user_id: @user.id, house_id: @house.id) if FavoriteHouse.first
+      if FavoriteHouse.find_by(user_id: @user.id, house_id: @house.id).nil?
+        FavoriteHouse.create(user_id: @user.id, house_id: @house.id) 
+      end
     end
 
     it 'should return all favorites' do
