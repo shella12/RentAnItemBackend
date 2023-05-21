bf14e3a97f163f902e854f3500e86359820fb2c6
Allassane <dickoallassane1997@gmail.com>
2023-05-21 20:06:32 +0100

    Fix linter

--- spec/controllers/api/v1/favorite_house_controller_spec.rb
+++ spec/controllers/api/v1/favorite_house_controller_spec.rb
@@ -1,6 +1,4 @@
 require 'rails_helper'
-require 'base64'
-# picture:{name:"picture", record:{ id:5, name:"Dream house", price:"30000.0", description:"Not my dream house",owner_name:"Junior Allassane", created_at:"2023-05-20T14:33:36.144Z",updated_at:"2023-05-20T14:33:36.242Z"}}, picture_url:"http://localhost:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBDZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--520ae9249b80f116d210ea7fca6d02256ade1345/NighMare%20House.webp",created_at:"2023-05-20T14:33:36.144Z", updated_at:"2023-05-20T14:33:36.242Z"}
 
 RSpec.describe Api::V1::FavoriteHousesController, type: :request do
   describe 'test api/v1/favorites endpoint' do
@@ -8,10 +6,6 @@ RSpec.describe Api::V1::FavoriteHousesController, type: :request do
       @user = User.find_by(email: 'test@gmail.com')
       @user ||= User.create(email: 'test@gmail.com', password: '123456')
       @house = House.first
-      # @house ||= House.create(name: 'house1', price: 100,
-      #  picture: Base64.strict_encode64(File.read(Rails.root.join('app', 'assets', 'images', 'images.jpeg'))),
-      #  description: 'description1',
-      #  owner_name: 'owner1')
       @house ||= House.new(
         name: 'House 1',
         price: 100_000,
