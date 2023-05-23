d58b01f411afa0fea3007a31a1d07f12f0267779
Allassane <dickoallassane1997@gmail.com>
2023-05-23 15:40:45 +0100

    Add production url to allowed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,6 +10,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
   
     allow do
+      origins "https://renthousefront.onrender.com/"
+      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
+
+    allow do
       origins "https://link-to-production-app.com/"
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
